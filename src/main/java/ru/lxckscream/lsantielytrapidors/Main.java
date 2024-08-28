988d8ea4ea0c633a2c40813d4a2416bcbfe6c588
Riflexx <mityafotografer@gmail.com>
2024-08-28 13:15:15 +0300

    build #3

--- src/main/java/ru/lxckscream/lsantielytrapidors/Main.java
+++ src/main/java/ru/lxckscream/lsantielytrapidors/Main.java
@@ -3,6 +3,7 @@ package ru.lxckscream.lsantielytrapidors;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -26,16 +27,21 @@ public final class Main extends JavaPlugin implements Listener {
             public void run() {
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     Inventory inventory = player.getInventory();
-                    if (inventory.contains(Material.ELYTRA)) {
-                        for (int i = 0; i < inventory.getSize(); i++) {
-                            if (inventory.getItem(i).getType() == Material.ELYTRA) {
+                        for (int i = 0; i < 45; i++) {
+                            ItemStack item = inventory.getItem(i);
+                            Material material = Material.AIR;
+                            try {
+                                material = item.getType();
+                            } catch (NullPointerException exception) {}
+
+                            if (material == Material.ELYTRA) {
                                 player.sendMessage(ChatColor.GOLD + "[Санкции] " + ChatColor.RED + "Элитры были удалены из вашего инвентаря.");
+                                player.playSound(player.getLocation(), Sound.BLOCK_LAVA_EXTINGUISH, 1, 1);
                                 inventory.setItem(i, new ItemStack(Material.AIR));
                             }
                         }
-                    }
                 }
             }
-        }.runTaskTimer(this, 20, 20);
+        }.runTaskTimer(this, 1, 1);
     }
 }
